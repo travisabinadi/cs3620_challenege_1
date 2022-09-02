8db1f99e04e851403664334c5b6a60945dfc48fa
ts920h <tsmith33@wayfair.com>
2022-09-02 15:10:57 -0600

    Completed Challenge 1

---
+++ main.py
@@ -0,0 +1,37 @@
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import math
+
+
+def calculate_simple_interest():
+    print("Calulating Simple Interest:\n")
+    p = int(input('Principle:  '))
+    n = int(input('Number of years:  '))
+    r = int(input('Rate of interest:  '))
+    simple_interest = (p*n*r)/100
+    print (f"Simple Interest: {simple_interest}")
+
+def lists():
+    favs: list[str] = ["Raspberries", "Ribs", "Rice", "Ramen", "Rib-Eye Steak"]
+    print(favs[2])
+    favs.extend(["Mac & Cheese", "Milkshake", "Mashed Potatoes"])
+    print(favs)
+    favs.insert(2, "Tacos")
+    print(favs)
+
+
+def for_and_square():
+    for i in range(0, 5):
+        print("I am a programmer.")
+    show_square()
+
+def show_square():
+    for i in range(1, 9):
+        print(f"{i} squared is {i*i}.")
+
+if __name__ == '__main__':
+    # Part 1
+    calculate_simple_interest()
+    # Part 2
+    lists()
+    # Part 3
+    for_and_square()
